3e5f9cb8db92834bfd1167e76a82d4e7bcf2aa17
immanual19 <immanuelsarker1@gmail.com>
2021-04-15 09:43:37 +0600

    fixed

--- src/components/Login/Login/firebase.config.js
+++ src/components/Login/Login/firebase.config.js
@@ -1,11 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAMNPBKgI1yqCg0JBUcoKKgjSU-v5l6IDc",
-    authDomain: "full-stack-client-immanual19.firebaseapp.com",
-    databaseURL: "https://full-stack-client-immanual19-default-rtdb.firebaseio.com",
-    projectId: "full-stack-client-immanual19",
-    storageBucket: "full-stack-client-immanual19.appspot.com",
-    messagingSenderId: "560971073942",
-    appId: "1:560971073942:web:9a1eead3022e72178cf5b7"
-  };
+  apiKey: "AIzaSyAw8NO_gxcobmlJzJ51ZOiMFsU2Ci0ztMo",
+  authDomain: "kidsheaven-e69b2.firebaseapp.com",
+  projectId: "kidsheaven-e69b2",
+  storageBucket: "kidsheaven-e69b2.appspot.com",
+  messagingSenderId: "731897255181",
+  appId: "1:731897255181:web:3f9e20442018c2a0244c44"
+};
 
   export default firebaseConfig;
