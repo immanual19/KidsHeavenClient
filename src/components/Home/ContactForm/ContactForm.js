206356c3960bb2a782bd8fb39767d27fe74addfa
immanual19 <immanuelsarker1@gmail.com>
2021-04-17 17:33:26 +0600

    finished

--- src/components/Home/ContactForm/ContactForm.js
+++ src/components/Home/ContactForm/ContactForm.js
@@ -4,22 +4,22 @@ const ContactForm = () => {
     return (
         <div class="contact-form-container">
         <h1>Contact Us</h1>
-        <p>Write us in details</p>
+        <p>Write us in details. Kids Heaven values every single feedback. Help us to take a better care of your child.</p>
         <form>
-          <label for="fname">First Name</label>
-          <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
+          
+          <input type="text" id="fname" name="firstname" placeholder="Your first name.."/>
       
-          <label for="lname">Last Name</label>
+         
           <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
       
-          <label for="country">Country</label>
+          
           <select id="country" name="country">
             <option value="australia">Bangladesh</option>
             <option value="canada">Canada</option>
             <option value="usa">USA</option>
           </select>
       
-          <label for="subject">Subject</label>
+          
           <textarea id="subject" name="subject" placeholder="Write something.." style={{height:'200px'}}></textarea>
       
           <input type="submit" value="Submit"/>
